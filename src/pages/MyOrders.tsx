2ee5124ab223684c4947ef7549397b8e0d5fafdb
shuvox33 <shuvox33@gmail.com>
2024-11-02 18:08:10 +0600

    create type and column for myorders page

--- src/pages/MyOrders.tsx
+++ src/pages/MyOrders.tsx
@@ -1,10 +1,40 @@
 
+import { ReactElement } from "react";
+import { Column } from "react-table";
+
+type DataType = {
+  _id: string;
+  amount: number;
+  quantity: number;
+  discount: number;
+  status: ReactElement;
+};
+
+const column: Column<DataType>[] = [
+  {
+    Header: "ID",
+    accessor: "_id",
+  },
+  {
+    Header: "Quantity",
+    accessor: "quantity",
+  },
+  {
+    Header: "Discount",
+    accessor: "discount",
+  },
+  {
+    Header: "Amount",
+    accessor: "amount",
+  },
+  {
+    Header: "Status",
+    accessor: "status",
+  },
+];
+
 const MyOrders = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  <div></div>
 };
 
 export default MyOrders;
